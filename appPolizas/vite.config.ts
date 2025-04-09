90b8825a3e9e3eb61d617b383195e560bee428cb
CH88320B <148910275+CH88320B@users.noreply.github.com>
2025-04-08 18:57:00 -0600

    Update vite.config.ts

--- appPolizas/vite.config.ts
+++ appPolizas/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 Importante para rutas relativas correctas en Azure
+  build: {
+    outDir: 'dist' // 👈 Carpeta de salida esperada por Azure Static Web App
+  }
 })
